5928998ecb77431d898fcf1f7ff83a4bd1c2fcd1
gyangupta773-sketch <gyangupta773@gmail.com>
2026-01-08 17:54:41 +0530

    Add files via upload

---
+++ DoubleCheckNumber.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number to check"))
+
+if(num > 50):
+    print(num,"is greater than 50")
+
+    if(num%2==0):
+        print(num,"is even")
+    else:
+        print(num,"is odd")
+
+    else:
+print(num,"less than 50")
